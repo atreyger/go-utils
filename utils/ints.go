649b54236a22da78206f47f98bff49f62abbddbc
Alex Treyger <alextreyger@invisionapp.com>
2019-12-19 23:20:32 -0800

    Add minInt, maxInt

--- utils/ints.go
+++ utils/ints.go
@@ -24,3 +24,17 @@ func Format64(n int64) string {
 		}
 	}
 }
+
+func minInt(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxInt(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
